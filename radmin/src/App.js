a22d727ee1b6bac5f114672be0da4f7ed5f2ac5a
zionHuang <hjszion@gmail.com>
2019-10-28 21:52:13 -0700

    update

--- radmin/src/App.js
+++ radmin/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+import {AuthLogin, SaveLoginUserInfo} from './Common/Auth';
 
 import Empty from './View/Empty';
 import Login from './View/Login';
@@ -16,6 +17,10 @@ function App() {
         }}></Route>
         <Route path="/home" render={(props) => {
           //校验当前用户是否已经登录 如果没有登录 跳转到登录页面
+          if(!AuthLogin()){
+            //跳转到登录页面前 记录当前用户要请求的页面 登录成功之后会跳转到用户之前要请求的地址
+            return <Redirect to="/login"></Redirect>
+          }
           //权限校验
           return <Home {...props}></Home>
         }}></Route>
--- radmin/src/Common/Auth.js
+++ radmin/src/Common/Auth.js
@@ -1,12 +1,24 @@
+//当前登录用户的sessionStorage的key = const APP_LOGIN_USER...
+const APP_LOGIN_USER = 'APP_LOGIN_USER';
+
 /**
  * 校验当前用户是否已经登录
  * return{Boolean} 如果已经登录:true 否则返回false
  */
 export function AuthLogin() {
     //sessionStorage 存储当前的登录信息
-    let loginUserStr = sessionStorage.getItem('APP_LOGIN_USER');
+    let loginUserStr = sessionStorage.getItem(APP_LOGIN_USER);
     if(loginUserStr){
         return true
     }
     return false;
+}
+
+/**
+ * 存储当前用户登录的信息到本地存储
+ * @param {Objec} user 
+ * @return undefined
+ */
+export function SaveLoginUserInfo(user){
+    sessionStorage.setItem(APP_LOGIN_USER, JSON.stringify(user));
 }
