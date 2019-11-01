e2687cc323e75711c774b8d43a7c28b373aa87b4
zionHuang <hjszion@gmail.com>
2019-11-01 00:03:06 -0700

    update

--- radmin/src/View/Admin/UserMgr/SetRole.js
+++ radmin/src/View/Admin/UserMgr/SetRole.js
@@ -1,37 +1,41 @@
 import React, { Component } from 'react';
-import { Modal } from 'antd';
+import { Modal, Checkbox, Row, Col } from 'antd';
 import service from '../../../Service';
 
 class SetRole extends Component {
     state = {
-        allRoles:[],   //所有的角色信息的数组
+        allRoles: [],   //所有的角色信息的数组
     }
-    componentDidMount(){
+    componentDidMount() {
         //加载所有的角色
         service.loadAllRoles()
-        .then(res => {
-            this.setState({allRoles: res.data});
-        })
+            .then(res => {
+                this.setState({ allRoles: res.data });
+            })
     }
     render() {
         return (
             <Modal
-               title="userSetRoles"
-               destroyOnClose
-               okText="set"
-               cancelText="cancel"
-               onCancel={() => this.props.close()}
-               visible={this.props.visible}
+                title="userSetRoles"
+                destroyOnClose
+                okText="set"
+                cancelText="cancel"
+                onCancel={() => this.props.close()}
+                visible={this.props.visible}
             >
                 <h3>setRole for User: {this.props.data ? this.props.data.name : null} </h3>
-                <hr/>
-                {
-                    this.state.allRoles.map(role => {
-                        return (
-                            <Checkbox>{role.name}</Checkbox>
-                        );
-                    })
-                }
+                <hr />
+                <Row>
+                    {
+                        this.state.allRoles.map(role => {
+                            return (
+                                <Col span={8}>
+                                    <Checkbox>{role.name}</Checkbox>
+                                </Col>
+                            );
+                        })
+                    }
+                </Row>
             </Modal>
         );
     }
