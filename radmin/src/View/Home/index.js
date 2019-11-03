235bba21bdeccc6cfa1fb6af2438f93f62a4bccd
zionHuang <hjszion@gmail.com>
2019-11-03 14:52:33 -0800

    update

--- radmin/src/View/Home/index.js
+++ radmin/src/View/Home/index.js
@@ -1,53 +1,49 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import { Route, Switch } from 'react-router-dom';
 import { Layout } from 'antd';
 import Top from '../../Components/Top';
 import MenuBar from '../../Components/MenuBar';
-import axios from 'axios';
-import { getLoginToken } from '../../Common/Auth';
-import {} from '../../Components/AuthRoute';
-import AuthRoute from '../../Components/AuthRoute';
-import GoodsMgr from '../GoodsMgr';
-
 import UserMgr from '../Admin/UserMgr';
 import RoleMgr from '../Admin/RoleMgr';
 import PerMgr from '../Admin/PerMgr';
-
-const {Header, Footer, Sider, Content} = Layout;
+import axios from 'axios';
+import { getLoginTocken } from '../../Common/Auth';
+import AuthRoute from '../../Components/AuthRoute';
+import GoodsMgr from '../GoodsMgr';
 
 class Home extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        //设置当前用户ajax请求的token
-        axios.defaults.headers['Authorization'] = getLoginToken();
+        // 设置当前用户ajax请求的token
+        axios.defaults.headers['Authorization'] = getLoginTocken();
         sessionStorage.removeItem('LOGIN_USER_PER');
     }
-    
+
     render() {
-        const {match} = this.props;
+        const { match } = this.props;
         return (
-            <Layout style={{height:'100vh'}}>
-                <Header style={{color:'#fff', padding:'0 15px'}}>
+            <Layout style={{ height: '100vh' }}>
+                <Header style={{ color: '#fff', padding: '0 15px' }}>
                     <Top history={this.props.history}></Top>
                 </Header>
-                <Layout style={{overflow: 'scroll'}}>
-                    <Sider style={{backgroundColor:'#fff'}}>
+                <Layout style={{ overflow: 'scroll' }}>
+                    <Sider style={{ backgroundColor: '#FFF' }}>
                         <MenuBar history={this.props.history}></MenuBar>
                     </Sider>
-                    <Content style={{padding:"15px"}}>
+                    <Content style={{ padding: '15px' }}>
                         <Switch>
-                            <AuthRoute per={} path={`${match.path}/user_mgr`} component={UserMgr}></AuthRoute>
-                            <AuthRoute per={} path={`${match.path}/role_mgr`} component={RoleMgr}></Route>
-                            <AuthRoute per={} path={`${match.path}/per_mgr`} component={PerMgr}></Route>
+                            <AuthRoute per={1570872984243} path={`${match.path}/user_mgr`} component={UserMgr}></AuthRoute>
+                            <AuthRoute per={1570873025381} path={`${match.path}/role_mgr`} component={RoleMgr}></AuthRoute>
+                            <AuthRoute per={1570873044424} path={`${match.path}/per_mgr`} component={PerMgr}></AuthRoute>
                             <Route path={`${match.path}/goods_mgr`} component={GoodsMgr}></Route>
-                            <Route >Welcome to use aicoder.com backend-management system</Route>
+                            <Route render={() => <h3>欢迎使用aicoder.com后台管理系统</h3>}></Route>
                         </Switch>
                     </Content>
                 </Layout>
-                <Footer style={{backgroundColor: 'silver', height: '24px', padding: '0', lineHeight: '24px'}}>Footer</Footer>
+                <Footer style={{ backgroundColor: 'silver', height: '24px', padding: '0', lineHeight: '24px' }}>底部</Footer>
             </Layout>
         )
     }
 }
 
-export default Home;
+export default Home
