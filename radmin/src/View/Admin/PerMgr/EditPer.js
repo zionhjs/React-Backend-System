0788c29a31cc4b23fde98bdd90361acec9a3837d
zionHuang <hjszion@gmail.com>
2019-11-03 19:40:02 -0800

    update

--- radmin/src/View/Admin/PerMgr/EditPer.js
+++ radmin/src/View/Admin/PerMgr/EditPer.js
@@ -2,44 +2,46 @@ import React, { Component } from 'react'
 import { Modal, Form, message } from 'antd'
 import EditPerFrm from './EditPerFrm';
 
-const EditPerFrmComponent = Form.create({name:'edit_per'})(EditPerFrm);
+const EditPerFrmComponent = Form.create({ name: 'edit_per' })(EditPerFrm);
 class EditPer extends Component {
     handleEditPer = () => {
         this.editFrm.validateFieldsAndScroll((err, values) => {
-            if(err){
-                message.error('please type right-data!');
+            if (err) {
+                message.error('please type-in correct data!');
                 return;
             }
             let newPer = Object.assign({}, this.props.data, values);
-            this.props.submitEditPer(newPer)
-            .then(res => {
-                message.info('edit success!');
-                this.props.close();
-            })
-            .catch(err => {
-                console.log('err:', err);
-                message.error('edit failed!');
-            })
+            console.log(newPer);
+            this.props
+                .submitEditPer(newPer)
+                .then(res => {
+                    message.info('edit success!');
+                    this.props.close();
+                })
+                .catch(err => {
+                    console.log('err :', err);
+                    message.error('edit failed!');
+                })
         });
     }
     editFrm = null;
     render() {
         return (
             <Modal
-               visble={this.props.visible}
-               destroyOnClose
-               okText="Edit"
-               cancelText="cancel"
-               onCancel={() => this.props.close()}
-               title="Edit-Auth"
-               onOk={this.handleEditPer}
+                visible={this.props.visible}
+                destroyOnClose
+                okText="edit"
+                cancelText="cancel"
+                onCancel={() => this.props.close()}
+                title="edit-Auth"
+                onOk={this.handleEditPer}
             >
-                <EditPerFrmComponent 
-                ref={frm => this.editFrm = frm}
-                   data={this.props.data} />
+                <EditPerFrmComponent
+                    ref={frm => this.editFrm = frm}
+                    data={this.props.data} />
             </Modal>
-        );
+        )
     }
 }
 
-export default EditPer;
+export default EditPer
--- radmin/src/View/Admin/PerMgr/EditPerFrm.js
+++ radmin/src/View/Admin/PerMgr/EditPerFrm.js
@@ -1,107 +1,110 @@
-import React, { Component } from 'react';
-import { Form, Input, Icon, message, Select, Option } from 'antd';
+import React, { Component } from 'react'
+import { Form, Input, Icon, Select } from 'antd'
 
 class EditPerFrm extends Component {
-    componentDidMount(){
-        let { data } = this.props;
-        this.props.form.setFieldsValue({
-            pId:data.pId, 
-            type:data.type,
-            order:data.order,
-            des:data.des,
-            code:data.code,
-            url:data.url
-        });
+    componentDidMount() {
+      let { data } = this.props;
+      this.props.form.setFieldsValue({
+        pId: data.pId, 
+        type: data.type, 
+        order: data.order,
+        des: data.des,
+        code: data.code,
+        url: data.url,
+      });
     }
-    render() {
-        const { getFieldDecorator } = this.props.form;
-        const { Option } = Select;
-        
-        return (
-            <Form
-                layout="horizontal"
-                labelCol={{ span: 4 }}
-                wrapperCol={{ span: 20 }}
-            >
-                <Form.Item lable="AuthName">
-                    {getFieldDecorator('des', {
-                        rules: [
-                            {
-                                min:2,
-                                max:20,
-                                message: 'please type-in 2~20 strings!',
-                            }, {
-                                required: true,
-                                message: 'Please type-in AuthName!',
-                            }
-                        ],
-                    })(
-                        <Input prefix={<Icon type="smile"></Icon>}
-                            placeholder="AuthName"
-                        />
-                    )}
-                </Form.Item>
-                <Form.Item lable="AuthType">
-                    {getFieldDecorator('type', {
-                        rules: [
-                            {
-                                required:true,
-                                message: 'please choose auth-type!',
-                            }],
-                    })(
-                        <Select defaultValue="router">
-                            <Option value="menu">MenuAuth</Option>
-                            <Option value="action">RequestAuth</Option>
-                            <Option value="router">RouterAuth</Option>
-                            <Option value="resource">ResourceAuth</Option>
-                            <Option value="component">ComponentAuth</Option>
-                        </Select>
-                    )}
-                </Form.Item>
-                <Form.Item lable="AuthCode">
-                    {getFieldDecorator('code', {
-                        rules: [
-                            {
-                                min:2,
-                                max:20,
-                                message: 'please type-in 2~20 strings!',
-                            }],
-                    })(
-                        <Input prefix={<Icon type="setting"></Icon>}
-                            placeholder="AuthCode"
-                        />
-                    )}
-                </Form.Item>
-                <Form.Item lable="ParentAuth">
-                    {getFieldDecorator('pId', {
-                        rules: [
-                            {
-                                required: true,
-                                message: 'Please type-in ParentAuth!',
-                            }],
-                    })(
-                        <Input prefix={<Icon type="user"></Icon>}
-                            placeholder="Parentauth"
-                        />
-                    )}
-                </Form.Item>
-                <Form.Item lable="Address">
-                    {getFieldDecorator('url')(
-                        <Input prefix={<Icon type="mail"></Icon>}
-                            placeholder="Parentauth"
-                        />
-                    )}
-                </Form.Item>
-                <Form.Item lable="Order">
-                    {getFieldDecorator('order')(
-                        <Input prefix={<Icon type="email"></Icon>}
-                            placeholder="Order"
-                        />
-                    )}
-                </Form.Item>
-            </Form>
-        );
+    render () {
+      const { getFieldDecorator } = this.props.form;
+      const { Option } = Select;
+      return (
+        <Form 
+        layout="horizontal"
+        labelCol={{span: 4}}
+        wrapperCol={{span: 20}}
+      >
+        <Form.Item label="Auth_Name">
+          {getFieldDecorator('des', {
+            rules: [
+              {
+                min: 2,
+                max: 20,
+                message: 'please type-in 2-20 strings!',
+              },
+              {
+                required: true,
+                message: 'please type-in Auth_Name',
+              }
+            ],
+          })(
+            <Input prefix={<Icon type="smile"></Icon>} 
+              placeholder="Auth_Name"
+            />
+          )}
+        </Form.Item>
+        <Form.Item label="Auth_Type">
+          {getFieldDecorator('type', {
+            rules: [
+              {
+                required: true,
+                message: 'please select Auth-Type!',
+              }
+            ],
+          })(
+            <Select>
+              <Option value="menu">Menu_Auth</Option>
+              <Option value="action">Request_Auth</Option>
+              <Option value="router">Route_Auth</Option>
+              <Option value="resource">Resource_Auth</Option>
+              <Option value="component">Component_Auth</Option>
+            </Select>
+          )}
+        </Form.Item>
+        <Form.Item label="Auth_Code">
+          {getFieldDecorator('code', {
+            rules: [
+              {
+                min: 2,
+                max: 20,
+                message: 'please type-in 2-20 strings!',
+              }
+            ],
+          })(
+            <Input prefix={<Icon type="setting"></Icon>} 
+              placeholder="Auth_Code"
+            />
+          )}
+        </Form.Item>
+        <Form.Item label="Parent_Auth">
+          {getFieldDecorator('pId', {
+            rules: [
+              {
+                required: true,
+                message: 'please type-in Parent_Auth!',
+              }
+            ],
+          })(
+            <Input prefix={<Icon type="user"></Icon>} 
+              placeholder="Parent_Auth"
+            />
+          )}
+        </Form.Item>
+        <Form.Item label="Address">
+          {getFieldDecorator('url')(
+            <Input prefix={<Icon type="mail"></Icon>} 
+              placeholder="Address"
+            />
+          )}
+        </Form.Item>
+        <Form.Item label="order">
+          {getFieldDecorator('order')(
+            <Input prefix={<Icon type="mail"></Icon>} 
+              placeholder="order"
+            />
+          )}
+        </Form.Item>
+      </Form>
+      )
     }
-}
-
-export default EditPerFrm;
+  }
+  
+  export default EditPerFrm
