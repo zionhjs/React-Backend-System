c46deabf6fa76fcf292b9d07275507281d60c7e0
zionHuang <hjszion@gmail.com>
2019-11-05 10:08:57 -0800

    update

--- radmin/src/store.js
+++ radmin/src/store.js
@@ -5,8 +5,8 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 
 const store = createStore(
     rootReducer,
-    {},
-    composeWithDevTools(applyMiddleware(thunk))
+    {},   //{} 里面包裹initial_state
+    composeWithDevTools(applyMiddleware(thunk))   //redux-devtools-extension 浏览器中的开发者工具
 );
 
 export default store;
